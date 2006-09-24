8f53e4cb9f477f80bae7d9b9bd3db10a45dc9af2
egor.margineanu <egor.margineanu@b2f71359-921d-0410-9b15-c5a7570799ef>
2006-09-24 14:41:52 +0000

    Added database and document class

---
+++ trunk/couchdb-lib/src/ro/egmar/couchdb/Database.java
@@ -0,0 +1,37 @@
+/** 
+ * CouchDB-lib is an open-source Java API for working with CouchDB.
+ * Copyright (C) 2006  Egor Margineanu (egor.margineanu@gmail.com)
+
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+package ro.egmar.couchdb;
+
+public class Database {
+    
+    /** Creates a new instance of Database */
+    public Database () {
+    }
+    
+    /** Return documents by key */
+    public List<Document> getDocumentsByKey(String key) {
+        return null;
+    }
+    
+    /** Return document by UNID */
+    public Document getDocumentByUNID(String unid) {
+        return null;
+    }
+}
